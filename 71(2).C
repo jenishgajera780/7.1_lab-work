eb0f6e68c99da9985c93d0a46f0e30e40d8374de
jenishgajera780 <136606761+jenishgajera780@users.noreply.github.com>
2023-07-06 14:09:38 +0530

    Update 71(2).C

--- 71(2).C
+++ 71(2).C
@@ -1,1 +1,39 @@
-#
+/*
+
+output ;
+
+12345
+12345
+12345
+12345
+12345
+
+*/
+
+
+
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+int j,i;
+
+for(i=1;i<=5;i++)
+{
+
+     for(j=1;j<=5;j++)
+{
+      
+     printf("%d",j);
+
+}
+
+printf("\n");
+
+}
+
+getch();
+
+}
